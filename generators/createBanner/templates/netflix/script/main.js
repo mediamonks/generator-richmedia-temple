e220a9c8af44c7513938f3b9f66e509a41a73fcb
Mient-jan Stelling <mientjan.stelling@gmail.com>
2018-10-23 18:49:30 +0200

    adding DoubleClickPlatformComponent #8 adding EventDispatcherComponent to addComponent

--- generators/createBanner/templates/netflix/script/main.js
+++ generators/createBanner/templates/netflix/script/main.js
@@ -1,6 +1,7 @@
 import Entity from '@mediamonks/temple/Entity';
 import EventDispatcherComponent from "@mediamonks/temple/component/EventDispatcherComponent";
 import MonetPlatformComponent from "@mediamonks/temple/component/platform/MonetPlatformComponent";
+import DoubleClickPlatformComponent from "@mediamonks/temple/component/platform/DoubleClickPlatformComponent";
 import ConfigComponent from "@mediamonks/temple/component/ConfigComponent";
 
 import config from '../.richmediarc';
@@ -12,6 +13,8 @@ class Banner extends Entity {
 
     this.addComponent(new ConfigComponent(config));
     this.addComponent(new MonetPlatformComponent());
+    this.addComponent(new DoubleClickPlatformComponent());
+    this.addComponent(new EventDispatcherComponent());
   }
 
   async init() {
