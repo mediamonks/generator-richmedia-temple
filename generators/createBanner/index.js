0aceace533a6b86bcf9b9422994bb6341ee0a498
Mient-jan Stelling <mientjan.stelling@gmail.com>
2019-05-14 11:37:27 +0200

    adding list

--- generators/createBanner/index.js
+++ generators/createBanner/index.js
@@ -13,15 +13,44 @@ module.exports = class extends Generator {
 
     this.result = {
       ...this.result,
-      ...await this.prompt([
-      {
-        type: 'input',
-        name: 'size',
-        message: 'Please fill in size of banner',
-        default: '300x250',
-        validate: input => /^\d+x\d+$/.test(input),
-      },
-    ])};
+      ...(await this.prompt([
+        {
+          type: 'select',
+          name: 'size',
+          message: 'Please select in size of banner',
+          default: '300x250',
+          choices: [
+            { name: 'Custom', value: 'custom' },
+            { name: '970x250 (Billboard)', value: '970x250' },
+            { name: '728x90 (Leaderboard)', value: '728x90' },
+            { name: '300x250 (Medium Rectangle)', value: '300x250' },
+            { name: '970x90 (Super Leaderboard)', value: '970x90' },
+            { name: '300x600 (Large Skyscraper)', value: '300x600' },
+            { name: '160x600 (Skyscraper)', value: '160x600' },
+            { name: '336x280', value: '336x280' },
+            { name: '320x240', value: '320x240' },
+            { name: '320x480', value: '320x480' },
+            { name: '300x50', value: '300x50' },
+            { name: '320x50', value: '320x50' },
+          ],
+        },
+      ])),
+    };
+
+    if (this.result.size === 'custom') {
+      this.result = {
+        ...this.result,
+        ...(await this.prompt([
+          {
+            type: 'input',
+            name: 'size',
+            message: 'Please select in size of banner',
+            default: '300x250',
+            validate: input => /^\d+x\d+$/.test(input),
+          },
+        ])),
+      };
+    }
 
     this.result = {
       ...this.result,
