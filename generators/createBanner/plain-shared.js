18ca8a4404885218be3fe6c7445991d509fff017
Paulie <paulie@mediamonks.com>
2021-01-08 14:21:30 +0100

    split up plain and plain shared in into two templates

---
+++ generators/createBanner/plain-shared.js
@@ -0,0 +1,76 @@
+const deepmerge = require('deepmerge');
+const mkdirp = require('mkdirp');
+
+const Generator = require('yeoman-generator');
+const isPathInside = require('is-path-inside');
+const path = require('path');
+
+const PlatformChoices = require('../../util/data/PlatformChoices');
+
+module.exports = class extends Generator {
+  async action() {
+    console.log("this.fs", this.fs)
+    console.log("this.fs.copyTpl", this.fs.copyTpl)
+    console.log("this.options", this.options)
+    const [width, height] = this.options.size.split('x');
+
+    this.fs.extendJSON(
+      this.destinationPath('package.json'),
+      this.fs.readJSON(this.templatePath('plain/extendPackageJson.json')),
+    );
+
+    // main html
+    this.fs.copyTpl(
+      this.templatePath('plain-shared/banner/index.html'),
+      this.destinationPath(path.join(this.options.outputPath, 'index.html')),
+      {
+        banner_width: width,
+        banner_height: height,
+      },
+    );
+
+    this.fs.copy(
+      this.templatePath('plain-shared/banner/static'),
+      this.destinationPath(path.join(this.options.outputPath,  'static')),
+    );
+
+    this.fs.copy(
+      this.templatePath('plain-shared/banner/img/**'),
+      this.destinationPath(path.join(this.options.outputPath), 'img/'),
+    );
+
+    // main javascript
+
+    this.fs.copy(
+      this.templatePath('plain-shared/banner/script'),
+      this.destinationPath(path.join(this.options.outputPath, 'script')),
+    );
+
+    this.fs.copy(
+      this.templatePath('plain-shared/shared/script'),
+      this.destinationPath(path.join(this.options.outputPath, '../../shared/', 'script')),
+    );
+
+    // copy pasting css
+    this.fs.copyTpl(
+      this.templatePath('plain-shared/banner/css/style.css'),
+      this.destinationPath(path.join(this.options.outputPath, 'css/style.css')),
+      {
+        banner_width: width,
+        banner_height: height,
+      },
+    );
+
+    const json = deepmerge(this.fs.readJSON(this.templatePath('plain-shared/banner/.richmediarc')), {
+      settings: {
+        size: {
+          width: parseInt(width, 10),
+          height: parseInt(height, 10),
+        },
+      },
+    });
+
+    this.fs.writeJSON(this.destinationPath(path.join(this.options.outputPath, '.richmediarc')), json);
+  }
+
+};
